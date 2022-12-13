7cc4426082e1e4d70fd97e502b87c5cff598a991
TheSeaweedMonster <105560604+TheSeaweedMonster@users.noreply.github.com>
2022-12-13 12:36:02 -0500

    Update manualmap.cs

--- manualmap.cs
+++ manualmap.cs
@@ -11,86 +11,85 @@ using System.Windows;
 
 namespace ManualMapApi
 {
-    public class imports
-    {
-        public const uint PAGE_NOACCESS = 0x1;
-        public const uint PAGE_READONLY = 0x2;
-        public const uint PAGE_READWRITE = 0x4;
-        public const uint PAGE_WRITECOPY = 0x8;
-        public const uint PAGE_EXECUTE = 0x10;
-        public const uint PAGE_EXECUTE_READ = 0x20;
-        public const uint PAGE_EXECUTE_READWRITE = 0x40;
-        public const uint PAGE_EXECUTE_WRITECOPY = 0x80;
-        public const uint PAGE_GUARD = 0x100;
-        public const uint PAGE_NOCACHE = 0x200;
-        public const uint PAGE_WRITECOMBINE = 0x400;
-
-        public const uint MEM_COMMIT = 0x1000;
-        public const uint MEM_RESERVE = 0x2000;
-        public const uint MEM_DECOMMIT = 0x4000;
-        public const uint MEM_RELEASE = 0x8000;
-
-        public const uint PROCESS_WM_READ = 0x0010;
-        public const uint PROCESS_ALL_ACCESS = 0x1F0FFF;
-
-        public const int EXCEPTION_CONTINUE_EXECUTION = -1;
-        public const int EXCEPTION_CONTINUE_SEARCH = 0;
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct MEMORY_BASIC_INFORMATION
-        {
-            public int BaseAddress;
-            public int AllocationBase;
-            public uint AllocationProtect;
-            public int RegionSize;
-            public uint State;
-            public uint Protect;
-            public uint Type;
-        }
-
-        [DllImport("kernel32.dll")]
-        public static extern int OpenProcess(uint dwDesiredAccess, bool bInheritHandle, int dwProcessId);
-
-        [DllImport("kernel32.dll")]
-        public static extern int ReadProcessMemory(int hProcess, int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
-
-        [DllImport("kernel32.dll")]
-        public static extern int WriteProcessMemory(int hProcess, int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesWritten);
-
-        [DllImport("kernel32.dll")]
-        public static extern int VirtualProtectEx(int hProcess, int lpBaseAddress, int dwSize, uint new_protect, ref uint lpOldProtect);
-
-        [DllImport("kernel32.dll")]
-        public static extern int VirtualQueryEx(int hProcess, int lpAddress, out MEMORY_BASIC_INFORMATION lpBuffer, uint dwLength);
-
-        [DllImport("kernel32.dll")]
-        public static extern int VirtualAllocEx(int hProcess, int lpAddress, int size, uint allocation_type, uint protect);
-
-        [DllImport("kernel32.dll")]
-        public static extern int VirtualFreeEx(int hProcess, int lpAddress, int size, uint allocation_type);
-
-        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
-        public static extern int GetModuleHandle(string lpModuleName);
-
-        [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
-        public static extern int GetProcAddress(int hModule, string procName);
-
-        [DllImport("kernel32.dll")]
-        public static extern uint GetLastError();
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        public static extern bool CloseHandle(int hObject);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool GetExitCodeProcess(int hProcess, out uint lpExitCode);
-
-        [DllImport("kernel32.dll")]
-        public static extern int CreateRemoteThread(int hProcess, int lpThreadAttributes, uint dwStackSize, int lpStartAddress, int lpParameter, uint dwCreationFlags, out int lpThreadId);
-    }
-
     public class MapInject
     {
+        private class imports
+        {
+            public const uint PAGE_NOACCESS = 0x1;
+            public const uint PAGE_READONLY = 0x2;
+            public const uint PAGE_READWRITE = 0x4;
+            public const uint PAGE_WRITECOPY = 0x8;
+            public const uint PAGE_EXECUTE = 0x10;
+            public const uint PAGE_EXECUTE_READ = 0x20;
+            public const uint PAGE_EXECUTE_READWRITE = 0x40;
+            public const uint PAGE_EXECUTE_WRITECOPY = 0x80;
+            public const uint PAGE_GUARD = 0x100;
+            public const uint PAGE_NOCACHE = 0x200;
+            public const uint PAGE_WRITECOMBINE = 0x400;
+
+            public const uint MEM_COMMIT = 0x1000;
+            public const uint MEM_RESERVE = 0x2000;
+            public const uint MEM_DECOMMIT = 0x4000;
+            public const uint MEM_RELEASE = 0x8000;
+
+            public const uint PROCESS_WM_READ = 0x0010;
+            public const uint PROCESS_ALL_ACCESS = 0x1F0FFF;
+
+            public const int EXCEPTION_CONTINUE_EXECUTION = -1;
+            public const int EXCEPTION_CONTINUE_SEARCH = 0;
+
+            [StructLayout(LayoutKind.Sequential)]
+            public struct MEMORY_BASIC_INFORMATION
+            {
+                public int BaseAddress;
+                public int AllocationBase;
+                public uint AllocationProtect;
+                public int RegionSize;
+                public uint State;
+                public uint Protect;
+                public uint Type;
+            }
+
+            [DllImport("kernel32.dll")]
+            public static extern int OpenProcess(uint dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+            [DllImport("kernel32.dll")]
+            public static extern int ReadProcessMemory(int hProcess, int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
+
+            [DllImport("kernel32.dll")]
+            public static extern int WriteProcessMemory(int hProcess, int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesWritten);
+
+            [DllImport("kernel32.dll")]
+            public static extern int VirtualProtectEx(int hProcess, int lpBaseAddress, int dwSize, uint new_protect, ref uint lpOldProtect);
+
+            [DllImport("kernel32.dll")]
+            public static extern int VirtualQueryEx(int hProcess, int lpAddress, out MEMORY_BASIC_INFORMATION lpBuffer, uint dwLength);
+
+            [DllImport("kernel32.dll")]
+            public static extern int VirtualAllocEx(int hProcess, int lpAddress, int size, uint allocation_type, uint protect);
+
+            [DllImport("kernel32.dll")]
+            public static extern int VirtualFreeEx(int hProcess, int lpAddress, int size, uint allocation_type);
+
+            [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+            public static extern int GetModuleHandle(string lpModuleName);
+
+            [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+            public static extern int GetProcAddress(int hModule, string procName);
+
+            [DllImport("kernel32.dll")]
+            public static extern uint GetLastError();
+
+            [DllImport("kernel32.dll", SetLastError = true)]
+            public static extern bool CloseHandle(int hObject);
+
+            [DllImport("kernel32.dll", SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool GetExitCodeProcess(int hProcess, out uint lpExitCode);
+
+            [DllImport("kernel32.dll")]
+            public static extern int CreateRemoteThread(int hProcess, int lpThreadAttributes, uint dwStackSize, int lpStartAddress, int lpParameter, uint dwCreationFlags, out int lpThreadId);
+        }
 
         public struct IMAGE_DATA_DIRECTORY
         {
@@ -587,7 +586,7 @@ namespace ManualMapApi
 
                         headerName += (char)buffer[j];
                     }
-
+                    
                     if (headerName == ".pdata" || headerName == ".rsrc" || headerName == ".reloc")
                     {
                         if (imports.WriteProcessMemory(handle, pTargetBase + (int)pSectionHeader.VirtualAddress, emptyBuffer2, (int)pSectionHeader.SizeOfRawData, ref nBytes) == 0)
